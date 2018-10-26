141d53909e0f6130ee5792bfae0a0439eb5b10ef
Blake Gotting <brgotting@gmail.com>
2018-10-26 15:55:24 -0700

    Restructure project.

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,19 @@
 
 
 //Business Logic
+function getArrayOfNumbersFromZeroToUserNumber(number){
 
 
-var zero = [0]
-var two = [2]
-var three = [3]
 //
 // //This function will take the inputted user number.
 
   var emptyArray = [];
 
-  var number = parseInt("#number")
-
   for(var i = 0; i <= number; i++) {
     console.log(i);
     emptyArray.push(i);
-
   };
-  // return emptyArray;
-//d
+}
 
 
 
@@ -37,7 +31,7 @@ var three = [3]
 //User Interface
 $(document).ready(function() {
   $("form#userInput").submit(function(event) {
-    var userInput = $("#number").val();
+    var number = $(parseInt("#number").val();
      var result = counter(userInput)
      $("#output").text(result);
      event.preventDefault();

467464260554640a9a5875e01d833850374c4630
Blake Gotting <brgotting@gmail.com>
2018-10-26 09:52:39 -0700

    Create function for creating array of numbers 0-userNumber.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,51 @@
+
+
+
+//Business Logic
+
+var numbersList = []
+var 0 = "beep"
+var 1 = "boop"
+var 3 = "I'm sorry Dave I cannot do that."
+
+//This function will take the inputted user number.
+
+function makeArrayOfNumbers(){
+
+  numbers.for(i = 0; i < userNumber.length; i++){
+  document.write('Item: ' + (i + 1) + ' of ' + userNumber.length);
+
+  numbersList.push(userNumber[i])
+
+}
+
+console.log(numbersList(5))
+
+
+});
+
+//And output an array of numbers n-user number.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//User Interface
 $(document).ready(function() {
 
 });
